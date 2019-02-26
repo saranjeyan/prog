68dec49ce07a6929620ce0322f949045efc5d23f
saranjeyan <43338185+saranjeyan@users.noreply.github.com>
2019-02-26 12:05:50 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("enter the year:");
+scanf("%d",&year);
+if(year%4==0)&&(year%100==0)||(year%400==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
