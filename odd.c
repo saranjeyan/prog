ab18e4ef95267372f1f8b7874fb65e8a3499da8f
saranjeyan <43338185+saranjeyan@users.noreply.github.com>
2019-03-05 12:36:01 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+printf("INPUT:");
+scanf("%d%d",&a,&b);
+printf("OUTPUT");
+fof(i=a;i<=b;i++)
+{
+if(i%2!=0)
+{
+printf("%d\t",i);
+}
+}
